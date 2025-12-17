ac115ba9a527e06b12220dd1d4ddbfc857d56227
DWI WASTI JUNIASIH <dwiwasti01@gmail.com>
2025-12-17 07:45:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1373278059-c3ddd",
+  "appId": "1:532301190326:web:c7968d344bfdffe962c217",
+  "apiKey": "AIzaSyB2KQSmzSvtaxSx7lLcpWtkb0K3h0J_-Oo",
+  "authDomain": "studio-1373278059-c3ddd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "532301190326"
+};
